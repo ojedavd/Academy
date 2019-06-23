9635ba595ea26bce08f06a52fe3ebf1e63277c0a
victor <victor@aula-ie.com>
2019-06-23 20:01:27 -0300

    Field edition

--- models/models.py
+++ models/models.py
@@ -6,6 +6,7 @@ class Teachers(models.Model):
     _name = 'academy.teachers'
 
     name = fields.Char()
+    biography = fields.Html()
     
 # class academy(models.Model):
 #     _name = 'academy.academy'
